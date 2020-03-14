3819e1ce738039decea7a8359219359975423594
Omer Cohen <omercohen213@gmail.com>
2020-03-14 12:35:27 +0200

    Create player.js

---
+++ js/player.js
@@ -0,0 +1,11 @@
+let player;
+
+function Player(classType, level, health, mana, strength, agility, speed) {
+  this.classType = classType;
+  this.level=level;
+  this.health = health;
+  this.mana = mana;
+  this.strength = strength;
+  this.agility = agility;
+  this.speed = speed;
+}
