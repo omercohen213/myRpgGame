1a23fda5ef16cb54b295aca9a7c700da13f5ab6a
Omer Cohen <omercohen213@gmail.com>
2020-03-14 16:08:52 +0200

    Update enemy.js

--- js/enemy.js
+++ js/enemy.js
@@ -1,12 +1,15 @@
 let enemy;
 
-function Enemy(enemyType, level, health, mana, strength, agility, speed, id) {
-  this.enemyType = enemyType;
-  this.level=level;
-  this.health = health;
-  this.mana = mana;
-  this.strength = strength;
-  this.agility = agility;
-  this.speed = speed;
-  this.id= id;
+class Enemy {
+  constructor(enemyType, level, health, mana, strength, agility, speed, id) {
+    this.enemyType = enemyType;
+    this.level = level;
+    this.health = health;
+    this.mana = mana;
+    this.strength = strength;
+    this.agility = agility;
+    this.speed = speed;
+    this.id = id;
+  }
 }
+
