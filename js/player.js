7c02075261e47f1f9fa409faf500f5dd5417c3e6
Omer Cohen <omercohen213@gmail.com>
2020-03-14 16:08:59 +0200

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,4 +1,6 @@
 let player;
+//let playerAttack;
+//let enemyAttack;
 
 function Player(classType, level, health, mana, strength, agility, speed) {
   this.classType = classType;
@@ -9,3 +11,36 @@ function Player(classType, level, health, mana, strength, agility, speed) {
   this.agility = agility;
   this.speed = speed;
 }
+
+let PlayerMoves = {
+  calcAttack() {
+    //who attacks first
+    let getPlayerSpeed= player.speed;
+    let getEnemySpeed= enemy.speed;
+    if (getPlayerSpeed>getEnemySpeed){
+      playerMoves.playerAttack();
+    }
+    //else enemyAttack();
+
+  },
+    //player attacks
+    playerAttack(){
+      let calcBaseDmg= player.strength * player.agility / 1000;
+      let offsetDmg= Math.floor(Math.random()*Math.floor(10));
+      let calcOutputDmg = calcBaseDmg + offsetDmg;     
+
+      let numOfHits= Math.floor(Math.random()*Math.floor(player.agility/20))+1;
+      let attackValues= [calcOutputDmg, numOfHits];
+      return attackValues;
+    },
+    //enemy attacks
+    enemyAttack(){
+      let calcBaseDmg= enemy.strength * enemy.agility / 1000;
+      let offsetDmg= Math.floor(Math.random()*Math.floor(10));
+      let calcOutputDmg = calcBaseDmg + offsetDmg;     
+
+      let numOfHits= Math.floor(Math.random()*Math.floor(enemy.agility/20))+1;
+      let attackValues= [calcOutputDmg, numOfHits];
+      return attackValues;
+    }
+ }
